3465016c00cab2057d45b32b5ec74a59e04fdd0a
Ye Ding <guiewy@gmail.com>
2022-07-04 17:04:24 +0800

    v0.7.4

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Webhook Trigger
 
-Webhook Trigger
+```bash
+usage: app.py [-h] [--debug] [--port PORT] path
+
+positional arguments:
+  path         monitoring git path, git will pull if triggered
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --debug      show debug information
+  --port PORT  listening port
+```
---
+++ app.py
@@ -0,0 +1,24 @@
+import argparse
+import logging
+import subprocess
+
+from flask import Flask
+from wrenchbox.logging import setup_log
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    s = subprocess.check_output(['git -C {} pull'.format(args.path)], shell=True)
+    return s.decode('UTF-8')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true', default=False, help='show debug information')
+    parser.add_argument('--port', type=int, default=8080, help='listening port')
+    parser.add_argument('path', type=str, help='monitoring git path, git will pull if triggered')
+    args, _ = parser.parse_known_args()
+    setup_log(level=logging.DEBUG if args.debug else logging.INFO)
+    app.run('0.0.0.0', args.port)
